bf3273c2cd3f0a6c7818cdea1de26d8244b2fc4e
nicolasbrest <nicolas.e.brest@gmail.com>
2021-04-11 21:04:54 -0300

    comit inicial

---
+++ EJ1/ej1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int counter=execute();
+    printFooter(counter);
+    return 0;
+}
+
+int execute(){
+    int counter=0;
+    int valor=0;
+    while (valor>=0) {
+        printf("Ingrese valores(negativo para finalizar): ");
+        //scanf tira lo que lee a una pos de memoria por eso el &
+        int ret=scanf("%d", &valor);
+        if(valor>0 && valor%2==0){
+            counter++;
+        }
+    }
+    return counter;
+
+}
+
+void printFooter(int c){
+    printf("Cantidad de pares: %d\n", c);
+    printf("Fin del programa.");
+}
